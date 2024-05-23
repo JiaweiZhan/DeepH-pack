bc970e9c470734e8db78a31c0ac9e99f0fa4a6d0
JiaweiZhan <jiaweiz@uchicago.edu>
2024-05-23 08:09:39 -0500

    g_orb

--- deeph/rotate.py
+++ deeph/rotate.py
@@ -50,7 +50,8 @@ class Rotate:
             0: torch.eye(1, dtype=torch_dtype).to(device=device),
             1: torch.eye(3, dtype=torch_dtype)[[1, 2, 0]].to(device=device),
             2: torch.eye(5, dtype=torch_dtype)[[2, 4, 0, 3, 1]].to(device=device),
-            3: torch.eye(7, dtype=torch_dtype)[[6, 4, 2, 0, 1, 3, 5]].to(device=device)
+            3: torch.eye(7, dtype=torch_dtype)[[6, 4, 2, 0, 1, 3, 5]].to(device=device),
+            4: torch.eye(9, dtype=torch_dtype)[[8, 6, 4, 2, 0, 1, 3, 5, 7]].to(device=device),
         }
         self.Us_wiki2openmx = {k: v.T for k, v in self.Us_openmx2wiki.items()}
 
